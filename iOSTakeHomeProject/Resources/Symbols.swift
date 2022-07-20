ecb3429fcf5228f26a96dbeeedd327138992ced9
JimCoder-Dev <jamesholmson88@gmail.com>
2022-07-20 16:23:15 +0100

    Initial setup

---
+++ iOSTakeHomeProject/Resources/Symbols.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+enum Symbols{
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
---
+++ iOSTakeHomeProject/iOSTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTakeHomeProjectApp.swift
+//  iOSTakeHomeProject
+//
+//  Created by James Holmson on 20/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOSTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
