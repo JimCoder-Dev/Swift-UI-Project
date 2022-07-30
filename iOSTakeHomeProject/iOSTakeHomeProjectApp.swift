7a415b910fbff879e6192950fd18a09ada215ffe
JimCoder-Dev <jamesholmson88@gmail.com>
2022-07-30 19:23:06 +0100

    Building the people view

--- iOSTakeHomeProject/iOSTakeHomeProjectApp.swift
+++ iOSTakeHomeProject/iOSTakeHomeProjectApp.swift
@@ -1,17 +1,16 @@
-//
-//  iOSTakeHomeProjectApp.swift
-//  iOSTakeHomeProject
-//
-//  Created by James Holmson on 20/07/2022.
-//
-
 import SwiftUI
 
 @main
 struct iOSTakeHomeProjectApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabView{
+                PeopleView()
+                    .tabItem{
+                        Symbols.person
+                        Text("Home")
+                    }
+            }
         }
     }
 }
